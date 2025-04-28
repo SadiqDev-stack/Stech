8d169b160a1d0d2d171245a93ce05233f9172a5f
SadiqDev-stack <sadiqmuh1321@gmail.com>
2025-04-28 06:28:21 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,22 +1,41 @@
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => response.ok ? response : new Response(`
-        <html>
-          <body>
-            <h1>Error Fetching Data</h1>
-            <p>Failed to fetch data.</p>
-          </body>
-        </html>
-      `, { headers: { 'Content-Type': 'text/html' } }))
-      .catch(error => new Response(`
-        <html>
-          <body>
-            <h1>Error Fetching Data</h1>
-            <p>Failed to fetch data.</p>
-          </body>
-        </html>
-      `, { headers: { 'Content-Type': 'text/html' } }))
+const cacheName = 'SADIQ TECH APP';
+const appAssets = [
+  '/showcase',
+  './showcase/profile.png',
+  './showcase/pump.png',
+  './showcase/ac_motor.png',
+  './showcase/all.png',
+  './showcase/career.png',
+  './showcase/coding.png',
+  './showcase/escavator.png',
+  './showcase/game.png',
+  './showcase/image.png',
+  './showcase/lamp.png',
+  './showcase/level_indicator.png',
+  './showcase/quiz.png',
+  './showcase/snet.png',
+  './showcase/weather.png',
+  'weather.json',
+  'index.html',
+  'worker.js',
+  'manifest.json',
+];
+
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(appAssets);
+    })
   );
-});
+})
+
+
+self.addEventListener('fetch',e => {
+  e.respondWith(
+     caches.match(e.request).then(response => {
+       return response ? response : fetch(e.request)
+     })
+    )
+})
